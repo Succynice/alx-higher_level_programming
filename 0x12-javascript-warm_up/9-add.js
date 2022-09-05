53dc1632656f4c9aea2a070163ccf4d0f1037691
succynice <succynice@gmail.com>
2022-09-06 00:16:42 +0100

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,11 +5,7 @@ function add (a, b) {
 }
 
 const entry = process.argv;
-const valueA = Math.floor(entry[2]);
-const valueB = Math.floor(entry[3]);
+const valueB = parseInt(entry[3]);
+const valueA = parseInt(entry[2]);
 
-if (isNaN(valueA) && isNaN(valueB)) {
-  console.log('Missing size');
-} else {
-  add(valueA, valueB);
-}
+add(valueA, valueB);
