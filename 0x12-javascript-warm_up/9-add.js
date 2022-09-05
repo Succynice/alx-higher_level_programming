2479075458e46e30d863e12bddfaf71ec6e618f7
succynice <succynice@gmail.com>
2022-09-06 00:19:05 +0100

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  console.log(a + b);
+  return(a + b);
 }
 
 const entry = process.argv;
 const valueB = parseInt(entry[3]);
 const valueA = parseInt(entry[2]);
 
-add(valueA, valueB);
+console.log(add(valueA, valueB));
