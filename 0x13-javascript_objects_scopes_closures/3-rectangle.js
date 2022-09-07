31061252545b208eb4eaa64f869fb0403da061be
succynice <succynice@gmail.com>
2022-09-07 14:47:13 +0100

    a class Rectangle that defines a rectangle:

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 // Prints a Rectangle with the parameters passed
 
-
 module.exports = class Rectangle {
-  constructor(width, height) {
+  constructor (width, height) {
     if (width === 'number' && height === 'number' && width > 0 && height > 0) {
       this.width = width;
       this.height = height;
     }
   }
 
-  print(c) {
-    c = 'X'
+  print (c) {
+    c = 'X';
     for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width))
+      console.log(c.repeat(this.width));
     }
   }
-}
+};
